fd81de0199f952d2ff28d20e5e3fbc8d024eeede
dicky hermawan <dhermawan215@gmail.com>
2024-12-11 17:06:41 +0700

    feat: finalisasi dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,12 +16,16 @@ class DashboardController extends Controller
         $transactionPending = Transaction::where('status_transaction', 0)->count();
         $transactionSuccess = Transaction::where('status_transaction', 1)->count();
         $transactionFail = Transaction::where('status_transaction', 2)->count();
+
+        $transactionData = Transaction::with(['transactionToProvider', 'transactionToPayment'])->limit(10)
+            ->orderBy('created_at', 'desc')->get();
         return \view('dashboard', [
             'providerCount' => $provider,
             'paymentCount' => $payment,
             'transactionSuccess' => $transactionSuccess,
             'transactionPending' => $transactionPending,
             'transactionFail' => $transactionFail,
+            'transactionData' => $transactionData,
         ]);
     }
 }
